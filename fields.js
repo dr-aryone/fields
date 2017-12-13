855bfbefec4a984a1917ac14bd21f0c78e08638f
Nathan Englert <nenglert@gmail.com>
2017-12-12 20:10:59 -0600

    Added some styling. Hooked up submit function.

--- fields.js
+++ fields.js
@@ -37,15 +37,11 @@
      * @param {String} [options.email]
      * @param {Array} [options.fields]
      */
-    function Module(options) {
+    function Module(email, options) {
         var inst = this;
         var settings;
 
         settings = inst._settings = mergeSettings(Module.defaultOptions, options);
-
-        if (!settings.email) {
-            throw new Error("Email is required when initializing a new Module instance.");
-        }
 
         if (settings.element) {
             settings.element = typeof settings.element === typeString 
@@ -61,7 +57,7 @@
             })
         }
 
-        inst._initialize();
+        inst._initialize(email);
     }
 
     /**
@@ -77,6 +73,18 @@
      */
 
     Module.defaultOptions = {
+        fields: [
+            {
+                label: "Email Address",
+                name: "email",
+                type: "email"
+            },
+            {
+                label: "Comments",
+                name: "comment",
+                type: "text"
+            }
+        ]
     };    
 
     /**
@@ -88,68 +96,113 @@
      * 
      * @private
      */
-    Module.prototype._initialize = function() {
+    Module.prototype._initialize = function(email) {
         var inst = this;
         var form;
 
-        form = inst._form = new Form(inst._settings.email);        
+        form = inst._form$ = new Form(email);        
 
         inst._settings.fields.forEach(function (fieldSettings) {
             var field = new Field(fieldSettings);
-            form.addField(field);
+            form.addField(fieldSettings.name, field);
         });        
 
-        inst._settings.element.onclick = function() {
-            console.log("HI");
-        }
+        inst._settings.element.onclick = () => inst._onOpenClick();
 
-        body.appendChild(form);
+        body.appendChild(form.getRootElement());
+    }
+
+    Module.prototype._onOpenClick = function () {
+        // TODO: Open modal window with form
+        console.log("HI");
     }
 
 
     function Form(email) {
         var inst = this;
-        var form, modal;
+        
+        inst._fields = {};
+        inst._formUrl = "https://formspree.io/"+email;
 
-        form = inst._form = doc.createElement("form");
-        form.action = "https://formspree.io/"+email;
-        form.method = "POST";        
+        var submit = doc.createElement("button");
+        submit.className = "fields-submit"
+        submit.innerText = "Send";
+        submit.type = "button";        
+        submit.onclick = () => inst._onSubmit();
 
-        modal = inst._modal = doc.createElement("div");
-        modal.id = "fields-modal"
+        var group = inst._fields$ = doc.createElement("group");
+        group.className = "fields-group";
+
+        var form = doc.createElement("div");
+        form.id = "fields-modal"
+        form.appendChild(group);  
+        form.appendChild(submit);   
+
+        var modal = inst._modal$ = doc.createElement("div");
+        modal.id = "fields-modal-wrapper";
         modal.appendChild(form);
-
-        return modal;
     }
 
-    Form.prototype.addField = function(field) {
+    Form.prototype.addField = function(key, field) {
         var inst = this;
+        inst._fields[key] = field;
+        inst._fields$.appendChild(field.getRootElement());
+    }
 
-        inst._form.appendChild(field);
+    Form.prototype.getRootElement = function() {
+        var inst = this;
+        return inst._modal$;
+    }
+
+    Form.prototype._onSubmit = function() {
+        var inst = this;
+        var data = {};
+        
+        Object.keys(inst._fields).forEach(function (key) {
+            data[key] = inst._fields[key].getValue();
+        });
+
+        var json = JSON.stringify(data);
+        
+        var request = new XMLHttpRequest();          
+        request.addEventListener('load', () => inst._onResponse());        
+        request.open('POST', inst._formUrl);                          
+        request.send(json);
+    }
+
+    Form.prototype._onResponse = function() {
+        console.log(this);
     }
 
 
     function Field(settings) {
         var inst = this;
-        var label, input, wrapper;
+        
+        var label = doc.createElement("label");
+        label.innerText = settings.label;
 
-        label = inst._label = doc.createElement("label");
-        label.innerText = settings.name;
-
-        input = inst._input = doc.createElement("input");
+        var input = inst._input$ = doc.createElement("input");
         input.name = settings.name;
         input.type = settings.type;
 
-        wrapper = inst._wrapper = doc.createElement("div");
-        wrapper.className = "fields-field";
-        wrapper.appendChild(label);
-        wrapper.appendChild(input);
+        var control = inst._control$ = doc.createElement("div");
+        control.className = "fields-control";
+        control.appendChild(label);
+        control.appendChild(input);
 
         if (settings.class && settings.class.length > 0) {
-            wrapper.classList.add(settings.class);
+            control.classList.add(settings.class);
         }
+    }
 
-        return wrapper;
+    Field.prototype.getRootElement = function() {
+        var inst = this;
+        return inst._control$;
+    }
+
+    Field.prototype.getValue = function() {
+        var inst = this;
+        return inst._input$.value;
     }
 
 
